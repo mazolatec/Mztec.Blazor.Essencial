8d404b44bbdc2378210a85cb3962d0f17c538047
Emilio a Mazola <mazolatec@gmail.com>
2025-10-25 13:03:06 -0300

    Adicionado ResumoTarefas apenas no Client e nos links

--- Mztec.BlazorTarefas.Shared/Entities/TarefaDados.cs
+++ Mztec.BlazorTarefas.Shared/Entities/TarefaDados.cs
@@ -1,12 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Mztec.BlazorTarefas.Shared.Data;
+
 
 namespace Mztec.BlazorTarefas.Shared.Entities
 {
-    internal class TarefaDados
+    public static class TarefaDados
     {
+        public static List<Tarefa> ObterTarefas() => new()
+  {
+     new Tarefa { Descricao = "Fazer curso",
+                  Concluida = false,
+                  DataCriacao = DateTime.Parse("2025-01-25")
+     },
+     new Tarefa { Descricao = "Estudar .NET 9",
+         Concluida = true,
+         DataCriacao = DateTime.Parse("2025-02-20")
+     },
+     new Tarefa { Descricao = "Ler documentação Blazor", Concluida = false,
+         DataCriacao = DateTime.Parse("2025-03-04")
+     },
+                 new Tarefa { Descricao = "Praticar consultas LINQ", Concluida = true, DataCriacao = DateTime.Parse("2025-04-09") },
+     new Tarefa { Descricao = "Criar API com EF Core", Concluida = false, DataCriacao = DateTime.Parse("2025-05-12") },
+     new Tarefa { Descricao = "Fazer testes unitários", Concluida = true, DataCriacao = DateTime.Parse("2025-06-01") },
+     new Tarefa { Descricao = "Publicar no GitHub", Concluida = false, DataCriacao = DateTime.Parse("2025-06-15") }
+   };
+
     }
 }
+
